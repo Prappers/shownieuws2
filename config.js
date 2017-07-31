1aa8950c525e68ff37fa1c2123bfee6179aae8d2
Prappers <30434976+Prappers@users.noreply.github.com>
2017-07-31 14:50:27 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'shownieuws',
+		wp_ws_url : 'http://degekte.nl/wp-appkit-api/shownieuws',
+		wp_url : 'http://degekte.nl',
+		theme : 'q-android',
+		version : '2.0.0',
+		app_title : 'Shownieuws',
+		app_platform : 'android',
+		gmt_offset : 1,
+		debug_mode : 'off',
+		auth_key : 'LwBARPYUUeHeWixmTzKYRucoNvzmXCwqirUVydVbVtfwFbtzBMqGkRQNahzrsWVQY',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
